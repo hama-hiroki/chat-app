7d58417e42ef88ca93f1a1be37f7d0140851ac51
hama-hiroki <trashman1214@icloud.com>
2022-09-10 15:57:55 +0900

    Messageモデルの作成とルーティングの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
 
-         validates :name, presence: true
-<<<<<<< HEAD
-
-         has_many :room_users
-         has_many :rooms, through: :room_users
-=======
->>>>>>> 33811109f06e025bed01e9d62ede9e0aa7a48e19
+  has_many :room_users
+  has_many :rooms, through: :room_users
 end
